f8a232cea9f8b7dc2b57e0ca31ce516303392025
mariguanero <mariguanero77@gmail.com>
2015-01-28 22:01:36 +0100

    Prueba1

--- core/src/com/majiba/geowallv2/GeoWallStart.java
+++ core/src/com/majiba/geowallv2/GeoWallStart.java
@@ -1,27 +1,34 @@
 package com.majiba.geowallv2;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 
-public class GeoWallStart extends ApplicationAdapter {
+public class GeoWallStart extends Game {
 	SpriteBatch batch;
-	Texture img;
+	Texture figuras;
+	TextureRegion miFigura;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		figuras = new Texture("Imagenes/figuras.png");
+		miFigura = new Sprite(figuras, 0, 256, 256, 256);
+		//miFigura.setPosition(0, 0);
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
+		Gdx.gl.glClearColor(1, 1, 1, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
-		batch.draw(img, 0, 0);
+		batch.draw(miFigura, Gdx.graphics.getWidth()/3, 0, Gdx.graphics.getWidth()/3, Gdx.graphics.getWidth()/3);
+		//batch.draw(img, 0, 0);
 		batch.end();
 	}
 }
